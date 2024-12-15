505090c3a1d13f61cd1c456a148ae90c09d06c6d
Yamada Taro <taro@yamada.com>
2024-12-15 17:25:32 +0100

    Implementierung der Kategorien für Traktanden schliesst #5

--- helper.py
+++ helper.py
@@ -2,15 +2,16 @@ import operator
 
 
 class TodoItem:
-    def __init__(self, title, date):
+    def __init__(self, title, date, category=None):
         self.title = title
         self.date = date
+        self.category = category
 
 
 # Liste für ToDo-Items
 items = []
 
 
-def add(title, date):
-    items.append(TodoItem(title, date))
+def add(title, date, category=None):
+    items.append(TodoItem(title, date, category))
     items.sort(key=operator.attrgetter("date"))
